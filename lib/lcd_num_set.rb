d03fe1086d03247e99e55cde5e68aae5c5914fb4
Rob Ryder <rob.ryder@sage.com>
2014-08-07 16:49:25 +0100

    add .valid_hash and #initialize methods and spec for LcdNumSet

--- lib/lcd_num_set.rb
+++ lib/lcd_num_set.rb
@@ -1,5 +1,19 @@
 class LcdNumSet
 
+  def self.valid_hash?(hash)
+    return false unless (hash.is_a?(Hash) && hash.keys==[:top,:middle,:bottom])
+    hash.each_value do |value|
+      return false unless (value.is_a?(String) && value.match(/^[ |_]{27}\n*$/))
+    end
+    return true
+  end
+  
+  
+  def initialize(hash)
+    
+    raise 'hash must be a hash of valid format' unless self.class.valid_hash?(hash)
+    
+  end
   
   
 end
--- spec/lcd_num_set_spec.rb
+++ spec/lcd_num_set_spec.rb
@@ -2,7 +2,6 @@ describe LcdNumSet do
 
   
   valid_test_string = '123456789'
-  valid_test_object = described_class.new
   valid_test_hash = {
       :top    => '    _  _     _  _  _  _  _ ',
       :middle => '  | _| _||_||_ |_   ||_||_|',
@@ -10,10 +9,35 @@ describe LcdNumSet do
   }
   
   
+  describe '.valid_hash?' do
+    
+    it 'returns false when given an array' do 
+      expect(described_class.valid_hash?([])).to be false
+    end
+
+    it 'returns false when given a hash of {:invalid=>"format"}' do
+      expect(described_class.valid_hash?({:invalid=>'format'})).to be false
+    end
+    
+    it 'returns true when given a hash of @valid_test_hash' do
+      expect(described_class.valid_hash?(valid_test_hash)).to be true
+    end
+    
+  end
+  
+  
   describe '#initialize' do
 
-    it "returns #{described_class} object when NOT given a variable" do
-      expect(described_class.new).to be_an_instance_of(described_class)
+    it 'raises an exception when given an array' do
+      expect{described_class.new([])}.to raise_error
+    end
+    
+    it 'raises an exception when given a hash of {:invalid=>"format"}' do
+      expect{described_class.new({:invalid=>'format'})}.to raise_error
+    end
+
+    it "returns a #{described_class} object when given a hash of @valid_test_hash" do
+      expect(described_class.new(valid_test_hash)).to be_an_instance_of(described_class)
     end
 
   end
