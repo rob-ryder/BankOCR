694608e9b7a21569b07006ff489ba6b51fad3671
Rob Ryder <rob.ryder@sage.com>
2014-08-08 11:17:48 +0100

    complete the LcdNumSet class and spec

--- lib/lcd_num_set.rb
+++ lib/lcd_num_set.rb
@@ -8,12 +8,65 @@ class LcdNumSet
     return true
   end
   
-  
-  def initialize(hash)
-    
-    raise 'hash must be a hash of valid format' unless self.class.valid_hash?(hash)
-    
+  def self.valid_string?(string)
+    return false unless (string.is_a?(String) && string.match(/^[0-9]{9}\n*$/))
+    return true
+  end
+
+  def self.split_set_hash_into_num_hashes(set_hash)
+    num_hashes = []
+    set_hash.each do |line_pos,line_content|
+      num_parts = line_content.scan(/[ |_]{3}/)
+      raise "Invalid count of number parts for line #{line_pos} in set_hash" unless num_parts.length==9
+      9.times do |i|
+        num_hashes[i] = {} if num_hashes[i]==nil
+        num_hashes[i][line_pos] = num_parts[i]
+      end
+    end
+    return num_hashes
   end
   
+  def initialize(numset,lcdNumClass=LcdNum)
+    if self.class.valid_hash?(numset) then
+      initialize_from_hash(numset,lcdNumClass)
+    elsif self.class.valid_string?(numset) then
+      initialize_from_string(numset,lcdNumClass)
+    else
+      raise 'numset must be a string or hash of valid format'
+    end
+  end
+  
+  def to_s
+    str = ''
+    @lcdnums.each do |lcdnum|
+      str << lcdnum.to_fixnum.to_s
+    end
+    return str
+  end
+  
+  def to_arr
+    arr = []
+    @lcdnums.each do |lcdnum|
+      arr << lcdnum.to_hash
+    end
+    return arr
+  end
+  
+  private
+  
+  def initialize_from_string(string, lcdNumClass)
+    @lcdnums = []
+    string.strip.each_char do |char|
+      @lcdnums << lcdNumClass.new(char.to_i)
+    end
+  end
+  
+  def initialize_from_hash(hash, lcdNumClass)
+    num_hashes = self.class.split_set_hash_into_num_hashes(hash)
+    @lcdnums = []
+    num_hashes.each do |num_hash|
+      @lcdnums << lcdNumClass.new(num_hash)
+    end
+  end
   
 end
--- spec/lcd_num_set_spec.rb
+++ spec/lcd_num_set_spec.rb
@@ -1,26 +1,101 @@
 describe LcdNumSet do
 
   
-  valid_test_string = '123456789'
-  valid_test_hash = {
+  valid_test_set_string = '123456789'
+  valid_test_set_hash = {
       :top    => '    _  _     _  _  _  _  _ ',
       :middle => '  | _| _||_||_ |_   ||_||_|',
       :bottom => '  ||_  _|  | _||_|  ||_| _|'
   }
-  
-  
+  valid_test_num_hash_array = [
+      {
+          :top    => '   ',
+          :middle => '  |',
+          :bottom => '  |'
+      },
+      {
+          :top    => ' _ ',
+          :middle => ' _|',
+          :bottom => '|_ '
+      },
+      {
+          :top    => ' _ ',
+          :middle => ' _|',
+          :bottom => ' _|'
+      },
+      {
+          :top    => '   ',
+          :middle => '|_|',
+          :bottom => '  |'
+      },
+      {
+          :top    => ' _ ',
+          :middle => '|_ ',
+          :bottom => ' _|'
+      },
+      {
+          :top    => ' _ ',
+          :middle => '|_ ',
+          :bottom => '|_|'
+      },
+      {
+          :top    => ' _ ',
+          :middle => '  |',
+          :bottom => '  |'
+      },
+      {
+          :top    => ' _ ',
+          :middle => '|_|',
+          :bottom => '|_|'
+      },
+      {
+          :top    => ' _ ',
+          :middle => '|_|',
+          :bottom => ' _|'
+      }
+  ]
+
+
   describe '.valid_hash?' do
-    
-    it 'returns false when given an array' do 
+
+    it 'returns false when given an array' do
       expect(described_class.valid_hash?([])).to be false
     end
 
     it 'returns false when given a hash of {:invalid=>"format"}' do
       expect(described_class.valid_hash?({:invalid=>'format'})).to be false
     end
+
+    it 'returns true when given a hash of @valid_test_set_hash' do
+      expect(described_class.valid_hash?(valid_test_set_hash)).to be true
+    end
+
+  end
+
+
+  describe '.valid_string?' do
+
+    it 'returns false when given an integer' do
+      expect(described_class.valid_string?(123456789)).to be false
+    end
+
+    it 'returns false when given a string of "invalid format"' do
+      expect(described_class.valid_string?('invalid format')).to be false
+    end
+
+    it 'returns true when given a string of @valid_test_set_string' do
+      expect(described_class.valid_string?(valid_test_set_string)).to be true
+    end
+
+  end
+
+
+  describe '.split_set_hash_into_num_hashes' do
     
-    it 'returns true when given a hash of @valid_test_hash' do
-      expect(described_class.valid_hash?(valid_test_hash)).to be true
+    it 'returns array of @valid_test_num_hash_array when given hash of @valid_test_set_hash' do
+      return_val = described_class.split_set_hash_into_num_hashes(valid_test_set_hash)
+      expect(return_val).to be_an_instance_of(Array)
+      expect(return_val).to eql(valid_test_num_hash_array)
     end
     
   end
@@ -36,11 +111,48 @@ describe LcdNumSet do
       expect{described_class.new({:invalid=>'format'})}.to raise_error
     end
 
-    it "returns a #{described_class} object when given a hash of @valid_test_hash" do
-      expect(described_class.new(valid_test_hash)).to be_an_instance_of(described_class)
+    it 'raises an exception when given a string of "invalid format"' do
+      expect{described_class.new('invalid format')}.to raise_error
+    end
+
+    it "returns a #{described_class} object when given a hash of @valid_test_set_hash" do
+      expect(described_class.new(valid_test_set_hash)).to be_an_instance_of(described_class)
+    end
+
+    it "returns a #{described_class} object when given a string of @valid_test_set_string" do
+      expect(described_class.new(valid_test_set_string)).to be_an_instance_of(described_class)
     end
 
   end
 
   
+  shared_examples "a lcd_num_set converter" do
+    describe '#to_s' do
+      it 'returns a string of value @valid_test_set_string' do
+        return_val = obj.to_s
+        expect(return_val).to be_an_instance_of(String)
+        expect(return_val).to eql(valid_test_set_string)
+      end
+    end
+    describe '#to_arr' do
+      it 'returns an array of value @valid_test_num_hash_array' do
+        return_val = obj.to_arr
+        expect(return_val).to be_an_instance_of(Array)
+        expect(return_val).to eql(valid_test_num_hash_array)
+      end
+    end
+  end
+
+  context 'an object initialized with a hash of @valid_test_set_hash' do
+    it_behaves_like 'a lcd_num_set converter' do
+       let(:obj){described_class.new(valid_test_set_hash)} 
+    end
+  end
+
+  context 'an object initialized with a hash of @valid_test_set_string' do
+    it_behaves_like 'a lcd_num_set converter' do
+      let(:obj){described_class.new(valid_test_set_string)}
+    end
+  end
+
 end
